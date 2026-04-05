382e7b3ae1f09edfdce25079ebc45c3a2c80073f
lerikal <valeria.listkova@gmail.com>
2026-04-05 13:25:47 +0200

    L2DU1: pridani novych trid Guest, Room a Booking

---
+++ src/Guest.java
@@ -0,0 +1,37 @@
+import java.time.LocalDate;
+
+public class Guest {
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+
+    public Guest(String firstName, String lastName, LocalDate dateOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+}
---
+++ src/Room.java
@@ -0,0 +1,57 @@
+import java.math.BigDecimal;
+
+public class Room {
+    private int roomNumber;
+    private int bedCount;
+    private boolean hasBalcony;
+    private boolean hasSeaView;
+    private BigDecimal pricePerNight;
+
+    public Room(int roomNumber, int bedCount, boolean hasBalcony, boolean hasSeaView, BigDecimal pricePerNight) {
+        this.roomNumber = roomNumber;
+        this.bedCount = bedCount;
+        this.hasBalcony = hasBalcony;
+        this.hasSeaView = hasSeaView;
+        this.pricePerNight = pricePerNight;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public int getBedCount() {
+        return bedCount;
+    }
+
+    public void setBedCount(int bedCount) {
+        this.bedCount = bedCount;
+    }
+
+    public boolean isHasBalcony() {
+        return hasBalcony;
+    }
+
+    public void setHasBalcony(boolean hasBalcony) {
+        this.hasBalcony = hasBalcony;
+    }
+
+    public boolean isHasSeaView() {
+        return hasSeaView;
+    }
+
+    public void setHasSeaView(boolean hasSeaView) {
+        this.hasSeaView = hasSeaView;
+    }
+
+    public BigDecimal getPricePerNight() {
+        return pricePerNight;
+    }
+
+    public void setPricePerNight(BigDecimal pricePerNight) {
+        this.pricePerNight = pricePerNight;
+    }
+}
